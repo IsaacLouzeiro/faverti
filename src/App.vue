105956a6fb33aa0f5ac845544342637301ac6d3d
IsaacLouzeiro <isaacelias1110@gmail.com>
2023-05-07 20:23:50 -0300

    Criando componente do Footer (Rodape)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
   import HeaderSection from './components/HeaderSection.vue';
+  import FooterSection from './components/FooterSection.vue';
 </script>
 
 <template>
   <HeaderSection />
 
   <RouterView />
+
+  <FooterSection />
 </template>
 
 <style scoped>
