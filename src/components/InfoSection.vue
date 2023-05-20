6839857960e93742c6d8b96ce0662246604685cc
IsaacLouzeiro <isaacelias1110@gmail.com>
2023-05-20 17:01:25 -0300

    criando o component sobre contribuir com TI Verde

--- src/components/InfoSection.vue
+++ src/components/InfoSection.vue
@@ -1,11 +1,14 @@
 <script setup>
   import BenefitsItGreenSection from './BenefitsItGreenSection.vue'
+  import ContributeItGreenSection from './ContributeItGreenSection.vue'
 </script>
 
 <template>
     <!-- ======= Info Section ======= -->
     <section id="info" class="info">
       <BenefitsItGreenSection />
+
+      <ContributeItGreenSection />
     </section><!-- End Info Section -->
 </template>
 
